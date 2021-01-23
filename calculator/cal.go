fa6159a9013619043dea1f8476bfbba5cdf734aa
jinah <jinah0818@gmail.com>
2021-01-24 00:26:04 +0900

    first commit

---
+++ calculator/cal.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main(){
+	var a int
+	var b int
+	a = 3
+	b = 4
+
+	fmt.Println(a+b)
+}
---
+++ hello/hello.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"	// fmt : go에서 제공하는 표준 패키지
+
+func main() {	// main 함수를 정의 (프로그램 시작점)
+	fmt.Println("hello world") // fmt 패키지의 Println 함수를 call
+}
